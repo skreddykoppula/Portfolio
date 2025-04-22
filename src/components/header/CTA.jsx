9b64e0d5aa1c3ee9f903c76ab0069a069814e2c5
Koppula Sai Krishna Reddy <skreddy.koppula@gmail.com>
2025-04-22 23:50:39 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,9 +4,6 @@ import CV from '../../assets/20881A0593.pdf';
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
-        Download Resume
-      </a>
       <a href="#contact" className="btn btn-primary">
         Let's talk
       </a>
